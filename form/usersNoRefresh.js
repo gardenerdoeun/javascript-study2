664bfd7c5a53f31ad9a17da2ce9461ee41fb8be2
gardenerdoeun <hneu8609@gmail.com>
2023-03-11 16:26:12 +0900

    div 태그 복사 & StringToObject

--- form/usersNoRefresh.js
+++ form/usersNoRefresh.js
@@ -1,11 +1,11 @@
 // 주소의 쿼리스트링 받아오기
 const queryString = new URLSearchParams(window.location.search);
-const nameText = queryString.get('input-text');
+const nameText = queryString.get('user-name');
 
 // // html 태그 내의 'input-text' name에 접근
 // const inputTextObjects = document.getElementsByName('input-text'); // 배열로 받아옴, 없으면 빈 배열을 받아옴
 // const inputTextObject = inputTextObjects[0];
-const inputTextObject = document.getElementsByName('input-text')[0];
+const inputTextObject = document.getElementsByName('user-name')[0];
 
 //input value 값 넣기
 inputTextObject.value = nameText;
@@ -31,10 +31,15 @@ const usersSet = function(){
 
 // Create
 const usersCreate = function (form){
-    const inputTextObject = form['input-text'];
-    users.push(inputTextObject.value);
+    const userNameObject = form['user-name'];
+    const userAgeObject = form['user-age'];
+    users.push({
+        name: userNameObject.value,
+        age: userAgeObject.value
+    });
+    userNameObject.value = '';
+    userAgeObject.value = '';
     usersSet();
-    inputTextObject.value = '';
     return usersRead();
 }; 
 
@@ -48,9 +53,12 @@ const usersRead = function() {
         tagDivParent.appendChild(newDivChild);
 
         const usersNameObject = document.getElementsByName('users-name')[index];
+        const usersAgeObject = document.getElementsByName('users-age')[index];
         const usersUpdateObject = document.getElementsByName('users-update')[index];
         const usersDeleteObject = document.getElementsByName('users-delete')[index];
-        usersNameObject.value = users[index];
+        
+        usersNameObject.value = users[index].name;
+        usersAgeObject.value = users[index].age;
         usersUpdateObject.index = index; // 제공되는 .index 속성은 없음 -> 단, 모든 태그는 object 타입이기 때문에 index라는 속성을 임시 생성
         usersDeleteObject.index = index;
     }
@@ -68,7 +76,15 @@ const usersDelete = function(index){
 // Update
 const usersUpdate = function(index){
     const name = document.getElementsByName('users-name')[index].value;
-    users[index] = name;
+    const age = document.getElementsByName('users-age')[index].value;
+    
+    // users[index].name = name;
+    // users[index].age = age;
+    users[index]= {
+        name : name,
+        age : age
+    };
+
     usersSet();
     return usersRead();
 }; 
